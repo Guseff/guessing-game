39264825e034eb78e667f2530b56645d68b23f67
Vasily Gusev <vasily.guseff@gmail.com>
2019-10-02 14:16:26 +0300

    feat: add task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+      this.start;
+      this.finish;
+    }
 
     setRange(min, max) {
-
+      this.start = min;
+      this.finish = max;
     }
 
     guess() {
-
+      return Math.round(this.start + (this.finish - this.start)/2);
     }
 
     lower() {
-
+      this.finish = Math.round(this.finish - (this.finish - this.start)/2);
     }
 
     greater() {
-
+      this.start = Math.round(this.start + (this.finish - this.start)/2);
     }
 }
 
